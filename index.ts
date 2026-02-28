d500a9adbf771421691f94680eba99a37b979463
josiaspiri <10800095+josiaspiri@users.noreply.github.com>
2026-02-27 19:43:36 -0500

    Use join on paths.

--- index.ts
+++ index.ts
@@ -87,7 +87,7 @@ const populateChannels = async (): Promise<void> => {
 };
 
 const loadChannelsFromIndex = async (): Promise<string[]> => {
-  const file = Bun.file("./index.json");
+  const file = Bun.file(join(CWD, "index.json"));
   if (!(await file.exists())) return [];
   try {
     const obj = await file.json() as Record<string, unknown>;
@@ -101,7 +101,7 @@ const loadChannelsFromIndex = async (): Promise<string[]> => {
 
 const loadState = async (): Promise<Partial<State> | null> => {
   try {
-    const file = Bun.file("state.json");
+    const file = Bun.file(join(CWD, "state.json"));
     if (!(await file.exists())) return null;
     const text = await file.text();
     return JSON.parse(text) as Partial<State>;
@@ -112,7 +112,7 @@ const loadState = async (): Promise<Partial<State> | null> => {
 
 const saveState = async (state: State): Promise<void> => {
   const json = JSON.stringify(state, null, 2);
-  await Bun.write("state.json", json);
+  await Bun.write(join(CWD, "state.json"), json);
 };
 
 const getPage = async (
