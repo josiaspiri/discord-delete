8719a33a9320afc35b7504049f38ce561d001b81
josiaspiri <10800095+josiaspiri@users.noreply.github.com>
2026-07-22 18:56:43 -0400

    Functional gateway with heartbeat.

--- src/gateway/index.ts
+++ src/gateway/index.ts
@@ -7,14 +7,41 @@ const socket = new WebSocket(
 socket.onopen = () => {
   const inflater = createInflate();
   let buffer = Buffer.alloc(0);
+  let heartbeatTimer: Timer | null = null;
+  let sequence: number | null = null;
+
+  const sendHeartbeat = (sequence: number | null): void => {
+    socket.send(JSON.stringify({
+      op: 40,
+      d: {
+        seq: sequence,
+        qos: { "active": false, "ver": 29, "reasons": [] },
+      },
+    }));
+  };
 
   inflater.on("data", (chunk) => {
     const rawData = chunk.toString("utf-8");
 
     try {
       const payload = JSON.parse(rawData);
+
+      if (payload.op === 0 && payload.s !== null) {
+        sequence = payload.s;
+      }
+
+      if (payload.op === 10 || payload.op === 11) {
+        if (heartbeatTimer) clearTimeout(heartbeatTimer);
+        const interval = payload.d.heartbeat_interval;
+        heartbeatTimer = setTimeout(() => sendHeartbeat(sequence), interval);
+      }
+
+      if (payload.op === 1) {
+        sendHeartbeat(sequence);
+      }
+
       console.log(payload);
-    } catch (_) {}
+    } catch (e) {}
   });
 
   socket.onmessage = (event) => {
@@ -30,8 +57,7 @@ socket.onopen = () => {
     buffer = Buffer.alloc(0);
   };
 
-  socket.send(JSON.stringify(
-    {
+  socket.send(JSON.stringify({
       "op": 2,
       "d": {
         "token": process.env.DISCORD_DELETE_AUTHORIZATION,
