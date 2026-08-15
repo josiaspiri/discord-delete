86573c1e686f720cd0deb56278cb7dacbcc99d82
josiaspiri <10800095+josiaspiri@users.noreply.github.com>
2026-08-15 07:13:28 -0400

    New/updated types.

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -2,5 +2,7 @@ declare namespace NodeJS {
   interface ProcessEnv {
     DISCORD_DELETE_AUTHORIZATION: string;
     DISCORD_DELETE_COOKIE: string;
+    EPHEMERAL_TTL_SECONDS?: string;
+    EPHEMERAL_SWEEP_INTERVAL_SECONDS?: string;
   }
 }
